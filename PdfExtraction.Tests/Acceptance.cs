985d204283d9ef5d5be507e8b8461ef4da224a75
Bao Vuong <bvuong@ncjua.com>
2017-10-01 12:08:41 -0400

    changed to nunit for cross platform things

---
+++ PdfExtraction.Tests/Acceptance.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VuongIdeas.PdfExtraction;
+
+namespace PdfExtraction.Tests
+{
+    [TestFixture]
+    public class Acceptance
+    {
+
+        [Test] public void PdfValidation1() => PdfValidationTemplate("Resources\\pdf-sample.pdf", "Adobe");
+        [Test] public void PdfValidation2() => PdfValidationTemplate("", "");
+
+        public void PdfValidationTemplate(string path, string text)
+        {
+            Assert.IsTrue(Path.Combine("", path).ExtractText().Contains(text));
+        }
+
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            var dir = Path.GetDirectoryName(typeof(Acceptance).Assembly.Location);
+            Directory.SetCurrentDirectory(dir);
+        }
+    }
+}
