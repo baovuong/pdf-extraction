9efe1b92d2abe66f528875cffba9b1866fa9a41c
Bao Vuong <bvuong@ncjua.com>
2017-10-01 02:25:19 -0400

    Add project files.

---
+++ PdfExtraction.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PdfExtraction.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PdfExtraction.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4bedc6f0-c7eb-40e4-ae01-9777b4594d1e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PdfExtraction/PdfExtensions.cs
@@ -0,0 +1,107 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.Advanced;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuongIdeas.PdfExtraction
+{
+    public static class PdfExtensions
+    {
+        public static string ExtractText(this string path)
+        {
+            using (var document = PdfReader.Open(path, PdfDocumentOpenMode.ReadOnly))
+            {
+                return document.ExtractText();
+            }
+        }
+
+        public static string ExtractText(this PdfDocument document)
+        {
+            var seenObjectNumbers = new HashSet<int>();
+            var result = new StringBuilder();
+            var objects = document.Internals.GetAllObjects();
+            foreach (var o in objects)
+            {
+                HandleObject(document, o, seenObjectNumbers, result);
+            }
+            return result.ToString();
+        }
+
+        private static void HandleObject(PdfDocument document, PdfObject value, HashSet<int> seenObjectNumbers, StringBuilder result)
+        {
+            if (value.GetType() == typeof(PdfDictionary))
+                HandleDictionary(document, (PdfDictionary)value, seenObjectNumbers, result);
+            else if (value.GetType() == typeof(PdfArray))
+                HandleArray(document, (PdfArray)value, seenObjectNumbers, result);
+        }
+
+        private static void HandleDictionary(PdfDocument document, PdfDictionary value, HashSet<int> seenObjectNumbers, StringBuilder target)
+        {
+            
+            if (value.Stream != null)
+            {
+
+            }
+            else
+            {
+                var elements = value.Elements.Values;
+                foreach (var element in elements)
+                {
+                    HandleItem(document, element, seenObjectNumbers, target);
+                }
+            }
+        }
+        
+        private static void HandleArray(PdfDocument document, PdfArray array, HashSet<int> seenObjectNumbers, StringBuilder target)
+        {
+            foreach (var element in array)
+            {
+                HandleItem(document, element, seenObjectNumbers, target);
+            }
+        }
+        private static void HandleItem(PdfDocument document, PdfItem item, HashSet<int> seenObjectNumbers, StringBuilder target)
+        {
+            if (item.GetType() == typeof(PdfObject)) HandleObject(document, (PdfObject)item, seenObjectNumbers, target);
+            else if (item.GetType() == typeof(PdfReference)) HandleReference(document, (PdfReference)item, seenObjectNumbers, target);
+            else if (item.GetType() == typeof(PdfString)) HandleString((PdfString)item, target);
+            else if (item.GetType() == typeof(PdfLiteral)) HandleLiteral((PdfLiteral)item, target);
+            else if (item.GetType() == typeof(PdfName)) HandleName((PdfName)item, target);
+            //else if (item.GetType() == typeof(PdfInteger)) HandleInteger((PdfInteger)item, target);
+            //else if (item.GetType() == typeof(PdfReal)) HandleReal((PdfReal)item, target);
+            //else if (item.GetType() == typeof(PdfBoolean)) HandleBoolean((PdfBoolean)item, target);
+        }
+
+        private static void HandleReal(PdfReal item, StringBuilder target) { target.Append(item.Value); }
+        private static void HandleInteger(PdfInteger item, StringBuilder target) { target.Append(item.Value); }
+        private static void HandleName(PdfName item, StringBuilder target) { target.Append(item.Value); }
+        private static void HandleNumber(PdfNumber item, StringBuilder target) { target.Append(item); }
+        private static void HandleLiteral(PdfLiteral item, StringBuilder target) { target.Append(item.Value); }
+        private static void HandleBoolean(PdfBoolean item, StringBuilder target) { target.Append(item.Value); }
+
+        private static void HandleString(PdfString text, StringBuilder target)
+        {
+            if (text.HexLiteral)
+            {
+                target.Append(text);
+            }
+            else
+            {
+                target.Append(text);
+            }
+        }
+
+        private static void HandleReference(PdfDocument document, PdfReference reference, HashSet<int> seenObjectNumbers, StringBuilder target)
+        {
+            if (!seenObjectNumbers.Contains(reference.ObjectNumber))
+            {
+                seenObjectNumbers.Add(reference.ObjectNumber);
+                HandleObject(document, reference.Value, seenObjectNumbers, target);
+            }
+        }
+    }
+}
